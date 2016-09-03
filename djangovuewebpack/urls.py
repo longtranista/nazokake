92bdfe14b7121c36dbb69fd8994f0a3303c8a725
maitvn <maitvn@maitvns-MacBook-Air.local>
2016-09-04 01:57:15 +0900

    update

--- djangovuewebpack/urls.py
+++ djangovuewebpack/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     url(r'^', include('myapp.urls')),
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
